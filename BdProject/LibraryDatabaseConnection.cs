280d0a280367c60f6af668bc0c1ee56bc491033d
Kamil Kaczmarczyk <kamilkaczm@gmail.com>
2012-04-11 17:25:06 +0200

    pierwszy commit

---
+++ BdProject/LibraryDatabaseConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Oracle.DataAccess.Client;
+using System.Data.Common;
+using System.Text;
+
+namespace BdProject
+{
+    class LibraryDatabaseConnection : IDisposable
+    {
+        private DbConnection connection;
+
+        public bool IsConnected
+        {
+            get
+            {
+                return (connection.State & ConnectionState.Open) != 0;
+            }
+        }
+
+        public LibraryDatabaseConnection()
+        {
+            connection = new OracleConnection(Properties.Settings.Default.ConnectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+
+        public string ExecuteTestCommand()
+        {
+            var command = connection.CreateCommand();
+            command.CommandText = "SELECT nazwa FROM kraje";
+            var reader = command.ExecuteReader();
+
+            var builder = new StringBuilder();
+
+            while (reader.Read())
+            {
+                builder.Append(reader["nazwa"]).Append("\r\n");
+            }
+            return builder.ToString();
+        }
+    }
+}
